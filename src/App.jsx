10c34b075a11fad317f3a0955da4de41a4984487
PedroBertha <phlikoskibertha@gmail.com>
2025-07-04 17:13:40 -0300

    salvando itens ao atualizar a pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export default function AddTarefa() {
-    const [tarefas, setTarefas] = useState([]);
+    const [tarefas, setTarefas] = useState(() => {
+        const savedTarefas = localStorage.getItem('tarefas')
+        return savedTarefas ? JSON.parse(savedTarefas) : [];
+    })
+
+    useEffect(() => {
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    },[tarefas])
 
     const addClick = () => {
         setTarefas([...tarefas, { done: false }]);
     };
 
     const deleteClick = id => {
-        setTarefas(tarefas => tarefas.filter((terefas, i) => i !== id))
+        setTarefas(tarefas => tarefas.filter((_terefas, i) => i !== id))
     }
 
     const toggleDone = id => {
@@ -27,16 +34,16 @@ export default function AddTarefa() {
     return (
         <div>
             <button onClick={addClick}>Adicionar tarefa</button>
-            {tarefas.map((tarefas, id) => (
-                <div key={id}>
+            {tarefas.map((tarefa, id) => (
+                <div key={tarefa.id}>
                     <input className='checkbox-input'
                         type="checkbox"
-                        checked={tarefas.done}
+                        checked={tarefa.done}
                         onChange={() => toggleDone(id)}
                     />
                     <input
                         type="text"
-                        value={tarefas.text}
+                        value={tarefa.text}
                         onChange={e => updateText(id, e.target.value)}
                     />
                     <button onClick={() => deleteClick(id)}>Deletar</button>
