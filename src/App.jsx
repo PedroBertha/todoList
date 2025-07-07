61f3d450920afa61c6f912991f606896ef666d33
PedroBertha <phlikoskibertha@gmail.com>
2025-07-07 15:02:45 -0300

    aplicando: darkmode, tarefas concluidas e total tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createContext, useMemo } from 'react'
 import './App.css'
+
+export const ThemeContext = createContext(null)
 
 export default function AddTarefa() {
     const [tarefas, setTarefas] = useState(() => {
@@ -9,7 +11,7 @@ export default function AddTarefa() {
 
     useEffect(() => {
         localStorage.setItem('tarefas', JSON.stringify(tarefas))
-    },[tarefas])
+    }, [tarefas])
 
     const addClick = () => {
         setTarefas([...tarefas, { done: false }]);
@@ -31,25 +33,58 @@ export default function AddTarefa() {
         )
     }
 
+    // Define o tema inicial como 'light'
+    const [theme, setTheme] = useState('light')
+
+    // Efeito colateral para aplicar a classe 'dark' ao body quando o tema for 'dark'
+    useEffect (() => {
+        document.body.className = theme === 'dark' ? 'dark' : ''
+    }, [theme])
+
+    // Calcula o número de tarefas concluídas 
+    const tarefasConcluidas = useMemo(() => {
+        return tarefas.filter(tarefas => tarefas.done).length
+    }, [tarefas])
+
+    // Calcula o número total de tarefas (contabiliza apaenas tarefas que tenham texto)
+    const totalTarefas = useMemo(() => {
+        return tarefas.filter(tarefas => tarefas.text).length
+    })
     return (
-        <div>
-            <button onClick={addClick}>Adicionar tarefa</button>
-            {tarefas.map((tarefa, id) => (
-                <div key={tarefa.id}>
-                    <input className='checkbox-input'
-                        type="checkbox"
-                        checked={tarefa.done}
-                        onChange={() => toggleDone(id)}
-                    />
+        <ThemeContext.Provider value={theme}>
+            <div className='app-container'>
+                <label>
                     <input
-                        type="text"
-                        value={tarefa.text}
-                        onChange={e => updateText(id, e.target.value)}
+                    type="checkbox"
+                    checked={theme === 'dark'}
+                    onChange={(e) => {
+                        setTheme(e.target.checked ? 'dark' : 'light')
+                    }}
                     />
-                    <button onClick={() => deleteClick(id)}>Deletar</button>
-                </div>
-            ))}
-        </div>
+                    Usar Modo Escuro
+                </label>
+                <hr />
+                <p>Total taredas {totalTarefas}</p>
+                <hr />
+                <p>Trefas Concluidas: {tarefasConcluidas}</p>
+                <button onClick={addClick}>Adicionar tarefa</button>
+                {tarefas.map((tarefa, id) => (
+                    <div key={tarefa.id}>
+                        <input className='checkbox-input'
+                            type="checkbox"
+                            checked={tarefa.done}
+                            onChange={() => toggleDone(id)}
+                        />
+                        <input
+                            type="text"
+                            value={tarefa.text}
+                            onChange={e => updateText(id, e.target.value)}
+                        />
+                        <button onClick={() => deleteClick(id)}>Deletar</button>
+                    </div>
+                ))}
+            </div>
+        </ThemeContext.Provider>
     );
 }
 
